f48b0ffc15f65758a85cb25b874d1280d2c8223c
sreeramnanna <93902408+sreeramnanna@users.noreply.github.com>
2024-09-09 16:48:30 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,9 @@ sudo apt install apache2 -y
 # Create an index.html file with "Hello, World!" content
 echo "<html>
 <head>
-  <title>Hello World</title>
+  <title>praveen babu thotatitle>
 </head>
 <body>
-  <h1>Hello, World!</h1>
+  <h1>praveen babu thota!</h1>
 </body>
 </html>" | sudo tee /var/www/html/index.html
